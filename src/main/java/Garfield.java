8710bab36d3b84451ea1830f7691bb46f87a085d
Faizal Rafi <syemfai@gmail.com>
2025-08-21 23:43:34 +0800

    Level 1

--- src/main/java/Garfield.java
+++ src/main/java/Garfield.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Garfield {
 
     public static void run(){
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Garfield");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Exits immediately
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        sc.close();
     }
 
     public static void main(String[] args) {
