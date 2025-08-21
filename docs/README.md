acb29da2c1ac90cb7c51e886d8bf33a3e7e1dd66
Faizal Rafi <syemfai@gmail.com>
2025-08-21 23:40:01 +0800

    Level 0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Garfield User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Garfield.java
@@ -0,0 +1,17 @@
+public class Garfield {
+
+    public static void run(){
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Garfield");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exits immediately
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+}
