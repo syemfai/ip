a16a1e58106e1b7b129dac33e248ac724875de90
Faizal Rafi <syemfai@gmail.com>
2025-09-16 23:52:17 +0800

    find

--- src/main/java/garfield/Ui.java
+++ src/main/java/garfield/Ui.java
@@ -1,5 +1,7 @@
 package garfield;
 
+import java.util.List;
+
 public class Ui {
     public void showLoadingError() {
         System.out.println("Error loading tasks from file.");
@@ -11,5 +13,19 @@ public class Ui {
 
     public void showGoodbye() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Displays the list of tasks found by the find command.
+     *
+     * @param foundTasks The list of matching tasks.
+     */
+    public void showFindResults(List<Task> foundTasks) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Here are the matching tasks in your list:");
+        for (int i = 0; i < foundTasks.size(); i++) {
+            System.out.println((i + 1) + "." + foundTasks.get(i));
+        }
+        System.out.println("____________________________________________________________");
     }
 }
