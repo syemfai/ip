f5ad5268f81f515f64a686d5a74ca71b705e5f0d
Faizal Rafi <syemfai@gmail.com>
2025-10-23 12:52:54 +0800

    Add constants

---
+++ src/main/java/garfield/util/Constants.java
@@ -0,0 +1,11 @@
+package garfield.util;
+
+public final class Constants {
+    public static final String LINE = "____________________________________________________________";
+    public static final String SEP_BY = "/by";
+    public static final String SEP_FROM = "/from";
+    public static final String SEP_TO = "/to";
+
+    private Constants() {
+    }
+}
